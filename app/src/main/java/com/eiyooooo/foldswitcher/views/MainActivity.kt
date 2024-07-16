34f170d039bd714b18d9ea4e0c654c12731bec6f
Kaiming Hu <huxxx1234@gmail.com>
2024-07-17 01:31:24 +0800

    Refactor MainActivity's Navigation based on screen size

--- app/src/main/java/com/eiyooooo/foldswitcher/views/MainActivity.kt
+++ app/src/main/java/com/eiyooooo/foldswitcher/views/MainActivity.kt
@@ -10,10 +10,11 @@ import com.eiyooooo.foldswitcher.R
 import com.eiyooooo.foldswitcher.adapters.MainViewPagerAdapter
 import com.eiyooooo.foldswitcher.databinding.ActivityMainBinding
 import com.eiyooooo.foldswitcher.helpers.ShizukuHelper
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val adapter by lazy { MainViewPagerAdapter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,49 +23,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewPager = binding.contentMain.mainViewPager
-        val adapter = MainViewPagerAdapter(this)
         viewPager.adapter = adapter
         viewPager.offscreenPageLimit = 4
 
-        val bottomNavigationView: BottomNavigationView = getBottomNavigationView(viewPager)
-        viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                bottomNavigationView.menu.getItem(position).setChecked(true)
-            }
-        })
+        val isCompactScreen = resources.configuration.screenWidthDp < 600
+        if (isCompactScreen) setupBottomNavigationView(viewPager)
+        else setupNavigationRail(viewPager)
 
         ShizukuHelper.checkShizukuPermission(0)
     }
 
-    private fun getBottomNavigationView(viewPager: ViewPager2): BottomNavigationView {
+    private fun setupBottomNavigationView(viewPager: ViewPager2) {
         val bottomNavigationView = binding.contentMain.bottomNavigation
+        if (bottomNavigationView == null) {
+            setupNavigationRail(viewPager)
+            return
+        }
 
         bottomNavigationView.setOnItemSelectedListener { item: MenuItem ->
-            val itemId = item.itemId
-            when (itemId) {
-                R.id.nav_home -> {
-                    viewPager.currentItem = 0
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.nav_manager -> {
-                    viewPager.currentItem = 1
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.nav_log -> {
-                    viewPager.currentItem = 2
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.nav_settings -> {
-                    viewPager.currentItem = 3
-                    return@setOnItemSelectedListener true
-                }
-
-                else -> false
+            when (item.itemId) {
+                R.id.nav_home -> viewPager.currentItem = 0
+                R.id.nav_manager -> viewPager.currentItem = 1
+                R.id.nav_log -> viewPager.currentItem = 2
+                R.id.nav_settings -> viewPager.currentItem = 3
             }
+            true
         }
-        return bottomNavigationView
+
+        viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
+    }
+
+    private fun setupNavigationRail(viewPager: ViewPager2) {
+        val navigationRailView = binding.contentMain.navigationRail
+        if (navigationRailView == null) {
+            setupBottomNavigationView(viewPager)
+            return
+        }
+
+        navigationRailView.setOnItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.nav_home -> viewPager.currentItem = 0
+                R.id.nav_manager -> viewPager.currentItem = 1
+                R.id.nav_log -> viewPager.currentItem = 2
+                R.id.nav_settings -> viewPager.currentItem = 3
+            }
+            true
+        }
+
+        viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                navigationRailView.menu.getItem(position).isChecked = true
+            }
+        })
     }
 }
