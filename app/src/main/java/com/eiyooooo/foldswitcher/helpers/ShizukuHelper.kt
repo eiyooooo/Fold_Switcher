1ea35f488180adb2fd8c1a96900857a6b4ce7bce
Kaiming Hu <huxxx1234@gmail.com>
2024-07-18 02:10:02 +0800

    Refactor ShizukuHelper to use object and improve state management

--- app/src/main/java/com/eiyooooo/foldswitcher/helpers/ShizukuHelper.kt
+++ app/src/main/java/com/eiyooooo/foldswitcher/helpers/ShizukuHelper.kt
@@ -2,47 +2,53 @@ package com.eiyooooo.foldswitcher.helpers
 
 import android.content.pm.PackageManager
 import com.eiyooooo.foldswitcher.types.ShizukuStatus
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import rikka.shizuku.Shizuku
 
-class ShizukuHelper {
-    companion object {
-        val shizukuStatus: MutableStateFlow<ShizukuStatus?> by lazy { MutableStateFlow(null) }
+object ShizukuHelper {
+    private val _shizukuStatus: MutableStateFlow<ShizukuStatus?> by lazy { MutableStateFlow(null) }
+    val shizukuStatus: Flow<ShizukuStatus?> = _shizukuStatus
 
-        fun setShizukuListener() {
-            Shizuku.addBinderReceivedListener {
-                if (shizukuStatus.value == ShizukuStatus.SHIZUKU_NOT_RUNNING)
-                    shizukuStatus.value = ShizukuStatus.SHIZUKU_RUNNING
-            }
-            Shizuku.addBinderDeadListener {
-                shizukuStatus.value = ShizukuStatus.SHIZUKU_NOT_RUNNING
-            }
-            Shizuku.addRequestPermissionResultListener { _, grantResult ->
-                if (grantResult == PackageManager.PERMISSION_GRANTED)
-                    shizukuStatus.value = ShizukuStatus.HAVE_PERMISSION
-                else
-                    shizukuStatus.value = ShizukuStatus.NO_PERMISSION
+    fun setShizukuListener() {
+        Shizuku.addBinderReceivedListener {
+            if (_shizukuStatus.value == ShizukuStatus.SHIZUKU_NOT_RUNNING) {
+                _shizukuStatus.value = ShizukuStatus.SHIZUKU_RUNNING
             }
         }
+        Shizuku.addBinderDeadListener {
+            _shizukuStatus.value = ShizukuStatus.SHIZUKU_NOT_RUNNING
+        }
+        Shizuku.addRequestPermissionResultListener { _, grantResult ->
+            if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                _shizukuStatus.value = ShizukuStatus.HAVE_PERMISSION
+            }
+            else {
+                _shizukuStatus.value = ShizukuStatus.NO_PERMISSION
+            }
+        }
+    }
 
-        fun checkShizukuPermission(code: Int) {
-            try {
-                if (Shizuku.isPreV11())
-                    shizukuStatus.value = ShizukuStatus.VERSION_NOT_SUPPORT
-                if (Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED)
-                    shizukuStatus.value = ShizukuStatus.HAVE_PERMISSION
-                else {
-                    Shizuku.requestPermission(code)
-                    shizukuStatus.value = ShizukuStatus.NO_PERMISSION
-                }
-            } catch (e: Exception) {
-                val msg = e.message
-                if (msg != null && msg.contains("binder haven't been received"))
-                    shizukuStatus.value = ShizukuStatus.SHIZUKU_NOT_RUNNING
-                else {
-                    shizukuStatus.value = ShizukuStatus.ERROR
-                    //TODO: log(Log.getStackTraceString(e))
-                }
+    fun checkShizukuPermission(code: Int) {
+        try {
+            if (Shizuku.isPreV11()) {
+                _shizukuStatus.value = ShizukuStatus.VERSION_NOT_SUPPORT
+            }
+            if (Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED) {
+                _shizukuStatus.value = ShizukuStatus.HAVE_PERMISSION
+            }
+            else {
+                Shizuku.requestPermission(code)
+                _shizukuStatus.value = ShizukuStatus.NO_PERMISSION
+            }
+        } catch (e: Exception) {
+            val msg = e.message
+            if (msg != null && msg.contains("binder haven't been received")) {
+                _shizukuStatus.value = ShizukuStatus.SHIZUKU_NOT_RUNNING
+            }
+            else {
+                _shizukuStatus.value = ShizukuStatus.ERROR
+                //TODO: log(Log.getStackTraceString(e))
             }
         }
     }
