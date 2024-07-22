93dc47a7a09252809b50426f32317209c6bbb53e
Kaiming Hu <huxxx1234@gmail.com>
2024-07-22 23:32:03 +0800

    Apply dynamic colors to activities if available

--- app/src/main/java/com/eiyooooo/foldswitcher/MyApplication.kt
+++ app/src/main/java/com/eiyooooo/foldswitcher/MyApplication.kt
@@ -2,11 +2,17 @@ package com.eiyooooo.foldswitcher
 
 import android.app.Application
 import android.content.Context
+import com.google.android.material.color.DynamicColors
 import org.lsposed.hiddenapibypass.HiddenApiBypass
 
 class MyApplication : Application() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         HiddenApiBypass.addHiddenApiExemptions("L")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
