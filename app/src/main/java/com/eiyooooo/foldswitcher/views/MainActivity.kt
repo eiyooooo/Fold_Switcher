64fe617b1b33e72951ba0c00625099208cdc17e6
Kaiming Hu <huxxx1234@gmail.com>
2024-08-03 01:16:39 +0800

    Add OptionsMenu

--- app/src/main/java/com/eiyooooo/foldswitcher/views/MainActivity.kt
+++ app/src/main/java/com/eiyooooo/foldswitcher/views/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.eiyooooo.foldswitcher.views
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.eiyooooo.foldswitcher.R
 import com.eiyooooo.foldswitcher.adapters.MainViewPagerAdapter
 import com.eiyooooo.foldswitcher.databinding.ActivityMainBinding
 import com.eiyooooo.foldswitcher.viewmodels.MainActivityViewModel
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import rikka.recyclerview.addEdgeSpacing
 import rikka.recyclerview.addItemSpacing
 import rikka.recyclerview.fixEdgeEffect
@@ -46,6 +53,35 @@ class MainActivity : AppCompatActivity() {
         recyclerView.addEdgeSpacing(top = 4f, bottom = 4f, left = 16f, right = 16f, unit = TypedValue.COMPLEX_UNIT_DIP)
 
         if (mainModel.useShizukuExecutor) mainModel.addShizukuListener()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.github -> {
+                try {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE)
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    intent.setData(Uri.parse("https://github.com/eiyooooo/Fold_Switcher"))
+                    startActivity(intent)
+                } catch (ignored: Exception) {
+                    Toast.makeText(this, getString(R.string.no_browser), Toast.LENGTH_SHORT).show()
+                }
+                true
+            }
+
+            R.id.licenses -> {
+                startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onResume() {
