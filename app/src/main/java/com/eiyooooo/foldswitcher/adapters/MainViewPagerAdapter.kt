54d4bb96980cf336f55a7c301775a5b9eff493c7
Kaiming Hu <huxxx1234@gmail.com>
2024-08-03 01:33:24 +0800

    Remove GitHubView

--- app/src/main/java/com/eiyooooo/foldswitcher/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/eiyooooo/foldswitcher/adapters/MainViewPagerAdapter.kt
@@ -7,7 +7,6 @@ import com.eiyooooo.foldswitcher.types.FoldStateData
 import com.eiyooooo.foldswitcher.types.ShizukuStatus
 import com.eiyooooo.foldswitcher.viewmodels.MainActivityViewModel
 import com.eiyooooo.foldswitcher.views.FoldStateViewHolder
-import com.eiyooooo.foldswitcher.views.GitHubViewHolder
 import com.eiyooooo.foldswitcher.views.InstructionQuickSwitchViewHolder
 import com.eiyooooo.foldswitcher.views.InstructionViewHolder
 import com.eiyooooo.foldswitcher.views.LicensesViewHolder
@@ -23,11 +22,10 @@ class MainViewPagerAdapter(private val mainActivity: MainActivity, private val m
         private const val ID_SHIZUKU_STATUS = 0L
         private const val ID_SHIZUKU_INSTRUCTION = 1L
         private const val ID_SHIZUKU_REQUEST = 2L
-        private const val ID_GITHUB = 3L
-        private const val ID_LICENSES = 4L
-        private const val ID_INSTRUCTION = 5L
-        private const val ID_INSTRUCTION_QUICK_SWITCH = 6L
-        private const val ID_RESET_STATE = 7L
+        private const val ID_LICENSES = 3L
+        private const val ID_INSTRUCTION = 4L
+        private const val ID_INSTRUCTION_QUICK_SWITCH = 5L
+        private const val ID_RESET_STATE = 6L
         private val idList = mutableListOf<Pair<Int, Long>>()
     }
 
@@ -57,7 +55,6 @@ class MainViewPagerAdapter(private val mainActivity: MainActivity, private val m
             }
         }
 
-        addItem(GitHubViewHolder.CREATOR, null, ID_GITHUB)
         addItem(LicensesViewHolder.CREATOR, null, ID_LICENSES)
 
         if (shizukuStatus == ShizukuStatus.HAVE_PERMISSION) {
@@ -73,7 +70,6 @@ class MainViewPagerAdapter(private val mainActivity: MainActivity, private val m
         mainModel.stopCheckingShizukuPermission()
         clear()
 
-        addItem(GitHubViewHolder.CREATOR, null, ID_GITHUB)
         addItem(LicensesViewHolder.CREATOR, null, ID_LICENSES)
 
         addStates()
@@ -85,7 +81,7 @@ class MainViewPagerAdapter(private val mainActivity: MainActivity, private val m
         addItem(InstructionViewHolder.CREATOR, null, ID_INSTRUCTION)
         addItem(InstructionQuickSwitchViewHolder.CREATOR, null, ID_INSTRUCTION_QUICK_SWITCH)
         addItem(FoldStateViewHolder.CREATOR, FoldStateData(this, mainModel, -1, mainActivity.getString(R.string.state_reset)), ID_RESET_STATE)
-        var id = 8L
+        var id = 7L
         mainModel.executor.value.supportStates.forEach { pair ->
             val name = pair.second.ifEmpty { pair.first.toString() }
             addItem(FoldStateViewHolder.CREATOR, FoldStateData(this, mainModel, pair.first, name), id)
